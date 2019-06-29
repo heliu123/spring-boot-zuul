359397c81b6e3c848c8cec1f5481bbb1cce9f59b
Administrator <heliu19871006@163.com>
2019-06-29 12:43:05 +0800

    first commit

---
+++ spring-cloud-gateway-example/src/main/java/com/example/springcloudgatewayexample/GatewayRoutes.java
@@ -0,0 +1,21 @@
+package com.example.springcloudgatewayexample;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class GatewayRoutes {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r ->
+                        r.path("/java/**")
+                                .filters(
+                                        f -> f.stripPrefix(1)
+                                )
+                                .uri("http://localhost:8090/helloWorld")
+                )
+                .build();
+    }
+}
---
+++ spring-cloud-gateway-example/src/main/java/com/example/springcloudgatewayexample/SpringCloudGatewayExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewayexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayExampleApplication.class, args);
+	}
+
+}
